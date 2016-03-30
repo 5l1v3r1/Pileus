caed4d22db6207dd71695470f1eb1cd4d2a2431f
sean <sjm4584@rit.edu>
2016-03-30 00:35:31 -0400

    changed it so the downloaded file has the name of the track in the title, cleaned up code a bit.

--- soundcloud_scraper.ps1
+++ soundcloud_scraper.ps1
@@ -8,12 +8,14 @@ $url_one = "http://api.soundcloud.com/resolve.json?url=https://soundcloud.com/us
 $resp = invoke-webrequest $url_one | select -Expand Content | ConvertFrom-Json
 $track_id = $resp[0].id
 
-write-host "/tracks/\$track_id\?client_id"
 # gets the download link for the track. You need to escape the variable being used apparently? hence \$track_id\
-$dl = Invoke-WebRequest "http://api.soundcloud.com/tracks/\$track_id\?client_id=$CLIENT_ID" | select -Expand Content | ConvertFrom-Json | select "download_url"
+$dl = Invoke-WebRequest "http://api.soundcloud.com/tracks/\$track_id\?client_id=$CLIENT_ID" | select -Expand Content | ConvertFrom-Json
+
+$title = $dl.title
+$fname = "testing_$title.wma"
 
 $tmp = "?client_id=$CLIENT_ID"
 $dl_url = $dl.download_url + $tmp
 
-write-host "---- $dl_url"
-invoke-webrequest $dl_url -OutFile "testing.wma"
+write-host "[+]URL --> $dl_url"
+invoke-webrequest $dl_url -OutFile $fname
