aa2b9584f33c0702afd951edd95f9036236e1885
Ubuntu <ubuntu@ip-172-31-47-154.us-west-2.compute.internal>
2016-04-03 22:33:57 +0000

    now reads creds from config with SafeConfigParser. *should* scp file over. Haven't tested it.

--- scripts/scrape_commander.py
+++ scripts/scrape_commander.py
@@ -5,6 +5,31 @@
 # the scrape_commander will then scp the file over, decrypt it, and deal with it
 
 import socket
+import threading
+import paramiko
+from ConfigParser import SafeConfigParser
+
+def createSSHClient(server, port, user, password):
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(server, port, user, password)
+    return client
+
+# scp's over <fname> from <ip>
+def worker(ip, fname):
+    # read config file for scp credsu
+    parser = SafeConfigParser()
+    parser.read('creds.ini')
+    uname = parser.get(ip, username)
+    passw = parser.get(ip, password)
+
+    ssh = createSSHClient(ip, 22, uname, passw)
+    scp = SCPClient(ssh.get_transport())
+    local_path = "/home/ubuntu/"
+    remote_path = fname
+    scp.get(local_path, remote_path)
+
 
 s = socket.socket()
 host = socket.gethostname()
@@ -17,5 +42,6 @@ while True:
     print 'got connection from: ', addr
     fname = c.recv(1024) # receive the filename info
     print addr[0], ":", fname 
+    t = threading.Thread(target=worker, args(addr[0], fname))
     c.close()
 
